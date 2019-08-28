573324fbf45901ee5fb8b013d33a9ce826a87977
zhouzq <18577496690@163.com>
2019-08-28 11:40:54 +0800

    公共包

---
+++ common-db/src/main/java/com/mengcc/db/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.mengcc.db.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        //解除分页限制
+        paginationInterceptor.setLimit(Long.MAX_VALUE);
+        return paginationInterceptor;
+    }
+
+    /**
+     * 乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
